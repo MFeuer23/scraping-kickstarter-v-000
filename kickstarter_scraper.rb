6e4c671bbc1020bb9786e8a26c0026fe32f2bf2b
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-28 18:18:36 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,8 +16,8 @@ require 'pry'
 
 create_project_hash
 
-# projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text
+# projects: kickstarter.css("li.project.grid_4")project.css("h2.bbcard_name strong a").text
+# title: 
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css(".project-card-wrap").text
