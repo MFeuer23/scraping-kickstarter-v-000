c170fc23af5aeca83d29dcc68eeeafb37bc66ee6
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-28 18:26:31 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,16 +4,25 @@ require 'pry'
 
 
   
-  html = File.read('fixtures/kickstarter.html')
+  
  
   
 
   def create_project_hash
     
-    
+    html = File.read('fixtures/kickstarter.html')
     kickstarter = Nokogiri::HTML(html)
+    
+    
+    projects = {}
+    
+    kickstarter.css("li.project.grid_4").each do |project|
+      projects[project] = {}
+    end
+      projects
+
   end
-  binding.pry
+  
 
 
 create_project_hash
