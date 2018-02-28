820169ddf133e20b074bf48dc45176723ef894c2
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-28 16:19:34 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,14 @@
 # require libraries/modules here
 require 'nokogiri'
 
+class KickstarterScraper
+  
+html = File.read('fixtures/kickstarter.html')
+ 
+kickstarter = Nokogiri::HTML(html)
+
 def create_project_hash
   # write your code here
+end
+
 end
